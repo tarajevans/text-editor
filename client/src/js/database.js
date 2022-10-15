ab160bbe1b446e6bd3d91fb5faed67f346c16778
Tara Evans <tarajevans@hotmail.com>
2022-10-15 13:14:54 -0400

    setup heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,5 @@
 import { openDB } from 'idb';
 
-
-
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -14,25 +12,24 @@ const initdb = async () =>
     },
   });
 
-// Add text to indexedDB
+// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-	const jateDB = await openDB('jate', 1);
-	const tx = jateDB.transaction('jate', 'readwrite');
-	const store = tx.objectStore('jate');
-	const request = store.put({ id: 1, value: content });
-	const result = await request;
-	console.log(result);
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ jate: content });
+  const result = await request;
+  console.log("Data saved to the database", result);
 };
 
-// Retrieve text from indexedDB
-export const getDb = async (e) => {
-	const jateDb = await openDB('jate', 1);
-	const tx = jateDb.transaction('jate', 'readonly');
-	const store = tx.objectStore('jate');
-	const request = store.get(1);
-	const result = await request;
-	return result?.value;
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log(result);
 };
-
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,59 +19,56 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin',
+        template: "./index.html",
+        title: "JATE",
       }),
+      // injects our custom service work from src-sw.js
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
+      // creates a manifest.json file
       new WebpackPwaManifest({
-        name: 'Text Editor Application',
-        short_name: 'Text Editor',
-        description: 'take notes!!',
-        background_color: '#7eb4e2',
-        theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text Editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join("assets", "icons"),
           },
-          {
-            src: path.resolve('src/images/logo.png'),
-            size: '1024x1024',
-            destination: path.join('assets', 'icons'),
-            purpose: 'maskable'
-          }
         ],
-      }),      
+      }),
     ],
 
     module: {
       rules: [
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.css$/i,
+          test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
-              ]
-            }
-          }
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
         }
+        
       ],
     },
   };
